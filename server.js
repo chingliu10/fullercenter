a701774fc109b920661e4b65b2c2a74edec43eb0
chingliu10 <56655368+chingliu10@users.noreply.github.com>
2025-06-11 21:45:02 +0300

    changed port to 7000

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const { engine } = require('express-handlebars');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 7000;
 
 // Configure Handlebars
 app.engine('handlebars', engine({
